620fce76f07194672d32e4797158ae770899f970
Pedro Salimon Nascimento <127759985+PedroSalimon@users.noreply.github.com>
2026-04-15 20:00:13 -0300

    continuando aula 8

--- ms-pagamentos/src/test/java/com/github/pedrosalimon/ms_pagamentos/service/PagamentoTestService.java
+++ ms-pagamentos/src/test/java/com/github/pedrosalimon/ms_pagamentos/service/PagamentoTestService.java
@@ -1,7 +1,10 @@
 package com.github.pedrosalimon.ms_pagamentos.service;
 
+import com.github.pedrosalimon.ms_pagamentos.dto.PagamentoDTO;
+import com.github.pedrosalimon.ms_pagamentos.enteties.Pagamento;
 import com.github.pedrosalimon.ms_pagamentos.exceptions.ResourceNotFoundException;
 import com.github.pedrosalimon.ms_pagamentos.repository.PagamentoRepository;
+import com.github.pedrosalimon.ms_pagamentos.tests.Factory;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +15,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class PagamentoTestService {
@@ -24,10 +29,14 @@ public class PagamentoTestService {
     private Long existingId;
     private Long nonExistingId;
 
+    private Pagamento pagamento;
+
     @BeforeEach
     void setUp() {
         existingId = 1l;
         nonExistingId = Long.MAX_VALUE;
+
+        pagamento = Factory.createPagamento();
     }
 
     @Test
@@ -60,4 +69,23 @@ public class PagamentoTestService {
         //anylong() é um matcher (coringa): aceita qualquer valor long/long
         Mockito.verify(pagamentoRepository, Mockito.never()).deleteById(Mockito.anyLong());
     }
+
+    @Test
+    void findPagamentoByIdShouldReturnPagamentoDTOWhenIdExists() {
+        //Arrange
+        Mockito.when(pagamentoRepository.findById(existingId))
+                .thenReturn(Optional.of(pagamento));
+
+        //Act
+        PagamentoDTO result = pagamentoService.findPagamentoById(existingId);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(pagamento.getId(), result.getId());
+        Assertions.assertEquals(pagamento.getValor(), result.getValor());
+
+        Mockito.verify(pagamentoRepository).findById(existingId);
+        Mockito.verifyNoMoreInteractions(pagamentoRepository);
+
+    }
+
+
 }
---
+++ ms-pagamentos/src/test/java/com/github/pedrosalimon/ms_pagamentos/tests/Factory.java
@@ -0,0 +1,22 @@
+package com.github.pedrosalimon.ms_pagamentos.tests;
+
+import com.github.pedrosalimon.ms_pagamentos.enteties.Pagamento;
+import com.github.pedrosalimon.ms_pagamentos.enteties.Status;
+
+import java.math.BigDecimal;
+
+public class Factory {
+
+    public static Pagamento createPagamento() {
+        Pagamento pagamento = new Pagamento(1L, BigDecimal.valueOf(32.25),
+                "Briannede Tarth", "7418529637412",
+                "07/15", "345", Status.CRIADO, 1L);
+        return pagamento;
+    }
+    public static Pagamento createPagamentoSemId() {
+        Pagamento pagamento = new Pagamento();
+        pagamento.setId(null);
+        return pagamento;
+    }
+
+}
